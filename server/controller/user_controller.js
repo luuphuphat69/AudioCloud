e1995290abfcecc0cbc2bb2e9b3318aa2a773fc5
Lưu Phú Phát <95926012+luuphuphat69@users.noreply.github.com>
2023-11-05 21:57:03 +0700

    fix cors

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -34,7 +34,7 @@ const userController = {
                         expiresIn: '1h',
                     });
                     // Save token into cookies
-                    res.cookie('token', token, { secure: false, maxAge: (60 * 60 * 24 * 30) * 1000, path: '/', domain:".54.206.75.221" });
+                    res.cookie('token', token, { secure: false, maxAge: (60 * 60 * 24 * 30) * 1000, path: '/', domain:".audiocloud.asia" });
                     return res.status(201).json(token);
                 } else {
                     console.log('Authentication failed');
