0f5b1861f11a27b6843809b04d652e1108dde795
luuphuphat69 <luuphuphat69@gmail.com>
2024-03-25 20:01:48 +0700

    fix update history

--- server/controller/history_controller.js
+++ server/controller/history_controller.js
@@ -9,34 +9,27 @@ const HistoryController = {
             const userId = req.params.userId;
             const audioId = req.params.audioId;
             const audio = await Audio.findOne({ AudioId: audioId });
-    
-            let history = await HistoryFlyweight.getUserHistory(userId);
-    
-            // If history is not found, create a new one
+
+            let history = await History.findOne({ UserId: userId });
+
             if (!history) {
                 history = new History({ UserId: userId, ListAudio: [] });
             }
-    
-            // Ensure history.ListAudio is initialized
-            if (!history.ListAudio) {
-                history.ListAudio = [];
-            }
-    
+
             const existingAudioIndex = history.ListAudio.findIndex((a) => a.AudioId === audioId);
-    
+
             if (existingAudioIndex !== -1) {
                 history.ListAudio[existingAudioIndex] = audio;
             } else {
                 history.ListAudio.push(audio);
             }
             await history.save();
-            return res.status(200).json({ message: "History updated successfully" });
+            return res.status(200);
         } catch (err) {
             console.error(err);
             return res.status(500).json({ message: "Server error" });
         }
     },
-        
     getHistory: async (req, res) => {
         const userId = req.params.userId;
         const history = await HistoryFlyweight.getUserHistory(userId)
